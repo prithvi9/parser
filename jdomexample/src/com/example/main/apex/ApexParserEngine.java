c973ebf52884f3f3936366a539925e8184b84730
prithvi9 <prithvi9@gmail.com>
2014-06-09 16:15:54 -0700

    mavenise

--- jdomexample/src/com/example/main/apex/ApexParserEngine.java
+++ jdomexample/src/com/example/main/apex/ApexParserEngine.java
@@ -1,55 +1,39 @@
-package com.example.main;
+package com.example.main.apex;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
-import org.jdom2.Namespace;
 import org.jdom2.filter.ElementFilter;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.util.IteratorIterable;
 
 import com.example.constants.WidgetTypes;
 import com.example.file.grep.TextSearch;
-import com.example.file.traversal.FileListingVisitor;
 import com.example.file.traversal.FileStats;
-import com.example.main.apex.ApexParserEngine;
 
-public class SFDCParserMain {
-
-	private Map<String, String> results = new HashMap<String, String>();
-	private static Attribute attribute = null;
-
-	public static void main(String[] args) {
-		FileListingVisitor flv = new FileListingVisitor();
-		FileStats fs = null;
-		try {
-			fs = flv.processDirectory("c:\\test");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		ApexParserEngine ape = new ApexParserEngine(fs);
-		ape.processVisualForcePage();
+public class ApexParserEngine implements ParserEngine{
+	private final FileStats fileStats;
+	public ApexParserEngine(FileStats fileStats) {
+		this.fileStats = fileStats;
 	}
 
-	/*private static void processVisualForcesPages(FileStats fs) {
+	@Override
+	public void processVisualForcePage() {
 		SAXBuilder builder = new SAXBuilder();
-		for (String s : fs.getPageLocations().keySet()) {
-			File xmlFile = new File(fs.getPageLocations().get(s));
+		for (String s : getFileStats().getPageLocations().keySet()) {
+			File xmlFile = new File(getFileStats().getPageLocations().get(s));
 			try {
 				Document document = (Document) builder.build(xmlFile);
 				Element rootNode = document.getRootElement();
 				List<Element> children = rootNode.getChildren("page");
-				attribute = rootNode.getAttribute("controller");
 				
-				findInputFields(rootNode, fs);
-				findInputTextAreaFields(rootNode,fs);
+				findInputFields(rootNode, getFileStats());
+				findInputTextAreaFields(rootNode,getFileStats());
+				find
 			} catch (IOException io) {
 				System.out.println(io.getMessage());
 			} catch (JDOMException jdomex) {
@@ -59,44 +43,54 @@ public class SFDCParserMain {
 
 	}
 
-	private static void findInputFields(Element rootNode, FileStats fs) {
+	private void findInputFields(Element rootNode, FileStats fs) {
 		ElementFilter inputTextAreaFilter = new ElementFilter(
 				WidgetTypes.widgetTypes.inputTextarea.name());
 		IteratorIterable<Element> descendants = rootNode
 				.getDescendants(inputTextAreaFilter);
-		processElements(descendants, fs);
+		processElements(descendants,rootNode, fs);
 	}
 
-	private static void findInputTextAreaFields(Element rootNode, FileStats fs) {
+	private void findInputTextAreaFields(Element rootNode, FileStats fs) {
 		ElementFilter inputFieldFilter = new ElementFilter(
 				WidgetTypes.widgetTypes.inputField.name());
 		IteratorIterable<Element> descendants = rootNode
 				.getDescendants(inputFieldFilter);
-		processElements(descendants, fs);
+		processElements(descendants,rootNode, fs);
 	}
-
-	private static void processElements(IteratorIterable<Element> descendants,  FileStats fs) {
+	
+	private void processElements(IteratorIterable<Element> descendants, Element rootNode, FileStats fs) {
 		while (descendants.hasNext()) {
 			Element c = (Element) descendants.next();
 			String[] splits = processField(c.getAttribute("value").getValue());
-			String controllerName = fs.getControllerLocations().get(attribute.getValue()+".cls");
+			String controllerName = fs.getControllerLocations().get(rootNode.getAttribute("controller").getValue()+".cls");
 			String className = null ;
 			try {
 				className = TextSearch.searchString(controllerName, splits[0]);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			System.out.println(attribute.getValue()+":"+className+":"+splits[1]);
+			System.out.println(rootNode.getAttribute("controller").getValue()+":"+className+":"+splits[1]);
 		}
 	}
 	
-
-	private static String[] processField(String value) {
+	private String[] processField(String value) {
 		value = value.replace("{", "").replace("}", "").replace("!", "");
 		if(value.indexOf(".")!=-1){
 			String[] split = value.split("\\.");
 			return split;
+		}else{
+			
 		}
 		return null;
-	}*/
+	}
+	
+	@Override
+	public void processStandardLayout() {
+		
+	}
+
+	public FileStats getFileStats() {
+		return fileStats;
+	}
 }
---
+++ jdomexample/src/com/example/main/apex/ParserEngine.java
@@ -0,0 +1,6 @@
+package com.example.main.apex;
+
+public interface ParserEngine {
+	public void processVisualForcePage();
+	public void processStandardLayout();
+}
