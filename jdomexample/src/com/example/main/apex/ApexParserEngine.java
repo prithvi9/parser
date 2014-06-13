eedc9f513b64ebe710332d15d320febf3bb44d58
prithvi9 <prithvi9@gmail.com>
2014-06-12 21:56:06 -0700

    added neko parser

--- jdomexample/src/com/example/main/apex/ApexParserEngine.java
+++ jdomexample/src/com/example/main/apex/ApexParserEngine.java
@@ -22,16 +22,17 @@ import com.example.constants.WidgetTypes.widgetTypes;
 import com.example.file.grep.TextSearch;
 import com.example.file.traversal.FileStats;
 
-public class ApexParserEngine implements ParserEngine{
+public class ApexParserEngine implements ParserEngine {
 	private static final Log log = LogFactory.getLog(ApexParserEngine.class);
 	private final FileStats fileStats;
+
 	public ApexParserEngine(FileStats fileStats) {
 		this.fileStats = fileStats;
 	}
 
 	@Override
-	public Map<String, Map<String,List<String>>> processVisualForcePage() {
-		Map<String, Map<String,List<String>>> results = new HashMap<String, Map<String,List<String>>> ();
+	public Map<String, Map<String, List<String>>> processVisualForcePage() {
+		Map<String, Map<String, List<String>>> results = new HashMap<String, Map<String, List<String>>>();
 		SAXBuilder builder = new SAXBuilder();
 		for (String s : getFileStats().getPageLocations().keySet()) {
 			File xmlFile = new File(getFileStats().getPageLocations().get(s));
@@ -40,106 +41,135 @@ public class ApexParserEngine implements ParserEngine{
 				Element rootNode = document.getRootElement();
 				processElements(s, rootNode, getFileStats(), results);
 			} catch (IOException io) {
-//				log.error(io.getMessage(), io);
+				// log.error(io.getMessage(), io);
 			} catch (JDOMException jdomex) {
-//				log.error(jdomex.getMessage(),jdomex);
+				// log.error(jdomex.getMessage(),jdomex);
 			}
 		}
 		return results;
 	}
 
-	
-	private void processElements(String pageName, Element rootNode, FileStats fs, Map<String, Map<String,List<String>>>  results) {
+	private void processElements(String pageName, Element rootNode,
+			FileStats fs, Map<String, Map<String, List<String>>> results) {
 		Attribute controllerNameInVfPage = null;
-		
-		if(rootNode.getAttribute("controller")!=null){
-			log.info("found Controller for "+ pageName);
+
+		if (rootNode.getAttribute("controller") != null) {
+			log.info("found Controller for " + pageName);
 			controllerNameInVfPage = rootNode.getAttribute("controller");
-		} else if (rootNode.getAttribute("standardController")!=null && rootNode.getAttribute("extensions")!=null){
-			log.info("found Standard controller, Extensions for "+ pageName);
+		} else if (rootNode.getAttribute("standardController") != null
+				&& rootNode.getAttribute("extensions") != null) {
+			log.info("found Standard controller, Extensions for " + pageName);
 			controllerNameInVfPage = rootNode.getAttribute("extensions");
-		} else if (rootNode.getAttribute("standardController")!=null){
-			log.info("found Standard controller for "+ pageName);
-			controllerNameInVfPage = rootNode.getAttribute("standardController");
+		} else if (rootNode.getAttribute("standardController") != null) {
+			log.info("found Standard controller for " + pageName);
+			controllerNameInVfPage = rootNode
+					.getAttribute("standardController");
 		}
-		for(WidgetTypes.widgetTypes type : widgetTypes.values()){
+		for (WidgetTypes.widgetTypes type : widgetTypes.values()) {
 			ElementFilter filter = new ElementFilter(type.name());
-			IteratorIterable<Element> descendants = rootNode.getDescendants(filter);
-			if (controllerNameInVfPage!=null && controllerNameInVfPage.getValue()!=null) {
-				String controllerClassName = fs.getControllerLocations().get(controllerNameInVfPage.getValue()+".cls");
+			IteratorIterable<Element> descendants = rootNode
+					.getDescendants(filter);
+			if (controllerNameInVfPage != null
+					&& controllerNameInVfPage.getValue() != null) {
+				String controllerClassName = fs.getControllerLocations().get(
+						controllerNameInVfPage.getValue() + ".cls");
 				while (descendants.hasNext()) {
 					Element c = (Element) descendants.next();
-					if(type.equals(widgetTypes.repeat)){
-						
-					}
-					else if (type.equals(widgetTypes.inputField)|| type.equals(widgetTypes.inputTextarea)){
-						String[] splits = processField(c.getAttribute("value").getValue());
+					if (type.equals(widgetTypes.repeat)) {
+						log.info(pageName + ":"
+								+ controllerNameInVfPage.getValue());
+					} else if (type.equals(widgetTypes.inputField)
+							|| type.equals(widgetTypes.inputTextarea)) {
+						String[] splits = processField(c.getAttribute("value")
+								.getValue());
 						if (controllerClassName != null) {
 							String className = null;
 							try {
-								className = TextSearch.searchString(controllerClassName,splits[0]);
+								className = TextSearch.searchString(
+										controllerClassName, splits[0]);
 							} catch (IOException e) {
 								log.error(e.getMessage(), e);
 							}
-							catalogResults(pageName, controllerClassName, className, splits[1], results);
-							log.info(pageName+":"+ controllerNameInVfPage.getValue() + ":" + className + ":"+ splits[1]);
-						}else{
-							log.info("Failed to fetch controller class name, probably a standard object");
-								String value = getValueAttribute(c);
-								
+							catalogResults(pageName, controllerClassName,
+									className, splits[1], results);
+							log.info(pageName + ":"
+									+ controllerNameInVfPage.getValue() + ":"
+									+ className + ":" + splits[1]);
+						} else {
+							catalogResults(pageName,
+									controllerNameInVfPage.getValue(),
+									splits[1], null, results);
+							log.info(pageName + ":"
+									+ controllerNameInVfPage.getValue() + ":"
+									+ splits[0] + ":" + splits[1]);
+
 						}
 					}
 				}
 			}
 		}
 	}
-	
-	private void catalogResults(String pageName, String controllerName, String fieldName, String s,  Map<String, Map<String,List<String>>>  results){
-		if(!results.containsKey(pageName)){
-			
+
+	private void catalogResults(String pageName, String controllerName,
+			String fieldName, String s,
+			Map<String, Map<String, List<String>>> results) {
+		if (!results.containsKey(pageName)) {
+
 			Map<String, List<String>> classDetails = new HashMap<String, List<String>>();
 			List<String> subfields = new ArrayList<String>();
-			subfields.add(fieldName+"^"+s);
+			if (s != null) {
+				subfields.add(fieldName + "^" + s);
+			} else {
+				subfields.add(fieldName);
+			}
 			classDetails.put(controllerName, subfields);
-			results.put(pageName,classDetails);
-			
-		}else{
+			results.put(pageName, classDetails);
+
+		} else {
 			Map<String, List<String>> map = results.get(pageName);
-			if(!map.containsKey(controllerName)){
+			if (!map.containsKey(controllerName)) {
 				Map<String, List<String>> classDetails = new HashMap<String, List<String>>();
 				List<String> subfields = new ArrayList<String>();
-				subfields.add(fieldName+"^"+s);
+				if (s != null) {
+					subfields.add(fieldName + "^" + s);
+				} else {
+					subfields.add(fieldName);
+				}
 				classDetails.put(controllerName, subfields);
-				results.put(pageName,classDetails);
-			}else{
+				results.put(pageName, classDetails);
+			} else {
 				List<String> list = map.get(controllerName);
-				list.add(fieldName+"^"+s);
+				if (s != null) {
+					list.add(fieldName + "^" + s);
+				} else {
+					list.add(fieldName);
+				}
 			}
 		}
 	}
-	
-	private String getValueAttribute(Element e){
-		if(e != null){
+
+	private String getValueAttribute(Element e) {
+		if (e != null) {
 			return e.getAttribute("value").getValue();
-		}else {
+		} else {
 			return null;
 		}
 	}
-	
+
 	private String[] processField(String value) {
 		value = value.replace("{", "").replace("}", "").replace("!", "");
-		if(value.indexOf(".")!=-1){
+		if (value.indexOf(".") != -1) {
 			String[] split = value.split("\\.");
 			return split;
-		}else{
-			
+		} else {
+
 		}
 		return null;
 	}
-	
+
 	@Override
 	public void processStandardLayout() {
-		
+
 	}
 
 	public FileStats getFileStats() {
